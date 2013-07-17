06923e11b323cbd923b49442cd0469e80d6a696f
gparlakov <gparlakov@gmail.com>
2013-07-17 02:19:28 -0700

    Deleted wrong results

--- 8.EntityFramework/NorthwindEntity.Model/NorthwindModelDiagram.Designer.cs
+++
@@ -1,4 +0,0 @@
-﻿// Default code generation is disabled for model 'C:\Users\student\Downloads\parlakov\trunk\8.EntityFramework\NorthwindEntity.Model\NorthwindModelDiagram.edmx'. 
-// To enable default code generation, change the value of the 'Code Generation Strategy' designer
-// property to an alternate value. This property is available in the Properties Window when the model is
-// open in the designer.
