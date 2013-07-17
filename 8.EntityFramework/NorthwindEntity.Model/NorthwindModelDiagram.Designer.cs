921ac489275655149f9b7f3bc3fa28f7d84a4d30
gparlakov <gparlakov@gmail.com>
2013-07-17 00:43:15 -0700

    Renamed diagram file

--- 8.EntityFramework/NorthwindEntity.Model/NorthwindModelDiagram.Designer.cs
+++ 8.EntityFramework/NorthwindEntity.Model/NorthwindModelDiagram.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Default code generation is disabled for model 'C:\Users\student\documents\visual studio 2012\Projects\EntityTest\NorthwindEntity.Model\Model1.edmx'. 
+﻿// Default code generation is disabled for model 'C:\Users\student\Downloads\parlakov\trunk\8.EntityFramework\NorthwindEntity.Model\NorthwindModelDiagram.edmx'. 
 // To enable default code generation, change the value of the 'Code Generation Strategy' designer
 // property to an alternate value. This property is available in the Properties Window when the model is
 // open in the designer.
